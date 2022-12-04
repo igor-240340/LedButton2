f7aa04f3ed12c721c328a13595943ba1e78d032e
igor <igor.240340@gmail.com>
2022-12-04 10:38:21 +0700

    Add INT1

--- LedButton2.asm
+++ LedButton2.asm
@@ -3,42 +3,43 @@
 .def tmp = r16
 .def ledArray = r20
 
-.equ START_PIN = 0                          ; Start button pin
+.equ START_PIN = 0                                          ; Start button pin
+.equ STOP_PIN = 0                                           ; Stop button pin
 
 .org 0x00
-                jmp Start
+                jmp Reset
+                jmp StartPressed
                 jmp StopPressed
 
-Start:          ldi tmp, low(RAMEND)        ; Stack Pointer
+Reset:          ldi tmp, low(RAMEND)                        ; Stack Pointer
                 out SPL, tmp
                 ldi tmp, high(RAMEND)
                 out SPH, tmp
 
-                ser tmp                     ; Potr B out
+                ser tmp                                     ; Potr B out
                 out DDRB, tmp
                 out PORTB, tmp
             
-                clr tmp                     ; Port D in
+                clr tmp                                     ; Port D in
                 out DDRD, tmp
-                ldi tmp, 0b00000101         ; Pull-up resistors
+                ldi tmp, 0b00001100                         ; Pull-up resistors
                 out PORTD, tmp
 
-                ldi tmp, (1 << INT0)        ; Enable INT0
+                ldi tmp, (1 << INT0 | 1 << INT1)            ; Enable INT0, INT1
                 out EIMSK, tmp
-                ldi tmp, (0b00 << ISC00)    ; By low level
+                ldi tmp, (0b00 << ISC00 | 0b00 << ISC10)    ; By low level
                 sts EICRA, tmp
-                sei                         ; Enable all interrupts
+
+                sei                                         ; Enable all interrupts
 
                 sec
-                set                         ; Left shift flag
+                set                                         ; Left shift flag
 
-                ldi ledArray, 0b11111110
+                ldi ledArray, 0b11011111
 
-WaitStart:      sbic PIND, START_PIN
-                rjmp WaitStart
-
-Loop:           out PORTB, ledArray
+MainLoop:       out PORTB, ledArray
                 rcall Delay500ms
+                ;rcall Delay0ms
                 
                 ser tmp
                 out PORTB, tmp
@@ -48,16 +49,24 @@ Loop:           out PORTB, ledArray
                 sbrs ledArray, 0
                 set
                 ror ledArray
-                rjmp Loop
+                rjmp MainLoop
 
-LeftShift:      sbrs ledArray, 5
-                clt                         ; Disable left shift
+EmptyLoop:      rjmp EmptyLoop
+
+LeftShift:      sbrc ledArray, 4
+                clt
+                sec
                 rol ledArray
-                rjmp Loop
+                rjmp MainLoop
 
-StopPressed:
-WaitStart2:     sbic PIND, START_PIN
-                rjmp WaitStart2
+StartPressed:   pop tmp
+                pop tmp
+                reti
+
+StopPressed:    ldi tmp, LOW(EmptyLoop)
+                push tmp
+                ldi tmp, HIGH(EmptyLoop)
+                push tmp
                 reti
 
 Delay500ms:     ldi r19, 100
@@ -70,4 +79,6 @@ Dec2:           dec r18
                 dec r19
                 brne Dec0
                 ret
+
+Delay0ms:       ret
 
